af2f355dda8f351dc99b5878b174249937a94eec
spikree <avimahalingpure10@gmail.com>
2025-06-15 00:48:51 +0530

    add patient details model

---
+++ src/models/patientdetails.model.ts
@@ -0,0 +1,36 @@
+import mongoose, { Document, Schema, Model } from "mongoose";
+
+export interface PatientDetails extends Document {
+  _id: mongoose.Types.ObjectId;
+  name: string;
+  patient: mongoose.Types.ObjectId;
+  doctor: mongoose.Types.ObjectId;
+  Disease: string;
+  symptom: string;
+  patientFeedback: string;
+  medicationPrescribed: string;
+  alergies: string;
+  createdOn: Date;
+}
+
+const PatientDetailsSchema: Schema<PatientDetails> = new Schema({
+  name: { type: String, required: true },
+  doctor: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  patient: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  Disease: { type: String, required: true },
+  symptom: { type: String, required: true },
+  patientFeedback: { type: String, required: true },
+  medicationPrescribed: { type: String, required: true },
+  alergies: { type: String, required: true },
+  createdOn: { type: Date, default: () => new Date() },
+});
+
+const PatientDetail: Model<PatientDetails> = mongoose.model<PatientDetails>(
+  "PatientDetail",
+  PatientDetailsSchema
+);
+export default PatientDetail;
