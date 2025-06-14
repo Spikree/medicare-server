a34d1dcba63fb32030530c0f09e6fa2d8d065b6e
spikree <avimahalingpure10@gmail.com>
2025-06-14 09:46:45 +0530

    add patient list model

---
+++ src/models/patientlist.model.ts
@@ -0,0 +1,29 @@
+import mongoose, {Document, Schema, Model} from "mongoose";
+
+export interface Patientlist extends Document {
+    _id: mongoose.Types.ObjectId;
+    name: string;
+    email: string;
+    bio: string;
+    profilePicture: string;
+    doctor: mongoose.Types.ObjectId;
+    patientStatus: "current"| "old";
+    createdOn: Date;
+}
+
+const PatientListSchema : Schema<Patientlist> = new Schema({
+    name:{type:String, required: true},
+    email: {type: String, required: true, unique: true},
+    bio:{type: String},
+    profilePicture: {type:String},
+    doctor:{type: mongoose.Schema.Types.ObjectId, ref:'User', required: true},
+    patientStatus: {
+        type: String,
+        required: true,
+        enum: ["current","old"]
+    },
+    createdOn: { type: Date, default: () => new Date() },
+});
+
+const PatientList : Model<Patientlist> = mongoose.model<Patientlist>("PatientList", PatientListSchema);
+export default PatientList;
--- src/models/user.models.ts
+++ src/models/user.models.ts
@@ -13,7 +13,7 @@ export interface User extends Document {
 }
 
 const userSchema: Schema<User> = new Schema({
-  name: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, unique: true },
   role: {
