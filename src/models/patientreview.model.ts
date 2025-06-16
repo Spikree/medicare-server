fd06218963edbfc219a18b92c8a292a266eea8c7
spikree <avimahalingpure10@gmail.com>
2025-06-16 19:20:05 +0530

    create patient review model for patient review on the digonisis and meds prescribed

---
+++ src/models/patientreview.model.ts
@@ -0,0 +1,36 @@
+import mongoose, { Document, Schema, Model } from "mongoose";
+
+export interface PatientReviews extends Document {
+  _id: mongoose.Types.ObjectId;
+  name: string;
+  patient: mongoose.Types.ObjectId;
+  doctor: mongoose.Types.ObjectId;
+  patientDetail: mongoose.Types.ObjectId;
+  patientReview: string;
+  sideEffects: string;
+  createdOn: Date;
+}
+
+const PatientReviewSchema: Schema<PatientReviews> = new Schema({
+  name: { type: String, required: true },
+  patient: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  doctor: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  patientDetail: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "PatientDetail",
+    required: true,
+  },
+  patientReview: { type: String, required: true },
+  sideEffects: { type: String },
+  createdOn: { type: Date, default: () => new Date() },
+});
+
+const PatientReview: Model<PatientReviews> = mongoose.model<PatientReviews>(
+  "PatientReview",
+  PatientReviewSchema
+);
+export default PatientReview;
