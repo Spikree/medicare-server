24d1b17f7637f0d0296027ae36027258b50f0b2c
spikree <avimahalingpure10@gmail.com>
2025-07-14 12:07:43 +0530

    minor change in patient list schema so that one doctor can add a patient only once

--- src/models/patientlist.model.ts
+++ src/models/patientlist.model.ts
@@ -1,31 +1,40 @@
-import mongoose, {Document, Schema, Model} from "mongoose";
+import mongoose, { Document, Schema, Model } from "mongoose";
 
 export interface Patientlist extends Document {
-    _id: mongoose.Types.ObjectId;
-    name: string;
-    email: string;
-    bio: string;
-    profilePicture: string;
-    patient: mongoose.Types.ObjectId;
-    doctor: mongoose.Types.ObjectId;
-    patientStatus: "current"| "old";
-    createdOn: Date;
+  _id: mongoose.Types.ObjectId;
+  name: string;
+  email: string;
+  bio: string;
+  profilePicture: string;
+  patient: mongoose.Types.ObjectId;
+  doctor: mongoose.Types.ObjectId;
+  patientStatus: "current" | "old";
+  createdOn: Date;
 }
 
-const PatientListSchema : Schema<Patientlist> = new Schema({
-    name:{type:String, required: true},
-    email: {type: String, required: true},
-    bio:{type: String},
-    profilePicture: {type:String},
-    doctor:{type: mongoose.Schema.Types.ObjectId, ref:'User', required: true},
-    patient:{type: mongoose.Schema.Types.ObjectId, ref:'User', required: true},
-    patientStatus: {
-        type: String,
-        required: true,
-        enum: ["current","old"]
-    },
-    createdOn: { type: Date, default: () => new Date() },
+const PatientListSchema: Schema<Patientlist> = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  bio: { type: String },
+  profilePicture: { type: String },
+  doctor: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  patient: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  patientStatus: {
+    type: String,
+    required: true,
+    enum: ["current", "old"],
+  },
+  createdOn: { type: Date, default: () => new Date() },
 });
 
-const PatientList : Model<Patientlist> = mongoose.model<Patientlist>("PatientList", PatientListSchema);
+PatientListSchema.index({ email: 1, doctor: 1 }, { unique: true });
+
+const PatientList: Model<Patientlist> = mongoose.model<Patientlist>(
+  "PatientList",
+  PatientListSchema
+);
 export default PatientList;
