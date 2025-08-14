466f53fbae2bf00720c46bba47fcb114e4eaf5fc
spikree <avimahalingpure10@gmail.com>
2025-08-14 13:28:00 +0530

    deployment fix #3 :sob:

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const app: Application = express();
 app.use(cookieParser());
 app.use(express.json());
 const port = process.env.PORT || 6000;
-app.use(cors({ origin: ["http://localhost:5173, https://medicare-client.onrender.com"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173", "https://medicare-client.onrender.com"], credentials: true }));
 connectDb();
 
 app.use("/auth", authRouter);
