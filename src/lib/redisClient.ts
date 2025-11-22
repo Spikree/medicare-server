0b4725c0fba0a124e3c8bca8759140449439eb4e
Spikree <avimahalingpure10@gmail.com>
2025-11-22 18:59:13 +0000

    redis client update

--- src/lib/redisClient.ts
+++ src/lib/redisClient.ts
@@ -1,11 +1,11 @@
 import { createClient } from "redis";
 
 const redisClient = createClient({
-  username: "default",
+  username: process.env.USER_NAME,
   password: process.env.REDIS_PASS,
   socket: {
     host: process.env.REDIS_HOST,
-    port: 16914,
+    port: 16147,
   },
 });
 
@@ -15,7 +15,13 @@ const redisClient = createClient({
 redisClient.on("error", (err) => console.log("Redis client error", err));
 
 async function connectRedis() {
-  await redisClient.connect();
+  try {
+    await redisClient.connect();
+    console.log("Redis connected");
+  } catch (error) {
+    console.error("Redis failed to connect, but the app is still running!");
+  }
+  
 }
 
 connectRedis();
